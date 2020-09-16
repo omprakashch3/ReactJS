4f497f38e80eb9c646f286bd0f6e6ef793fde1a3
omprakashch3 <omprakashchautala3@gmail.com>
2020-09-16 10:28:42 +0530

    importing component from other files

--- hello/src/App.js
+++ hello/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
@@ -8,29 +9,6 @@ function App() {
       <Header />
     </div>
   );
-}
-
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
 }
 
 export default App;
